5c4b7df46a348ba0c738c1528a27c97ef60e5c41
Oliver Joos <oliver.joos@hispeed.ch>
2018-05-14 18:02:34 +0200

    Replace re.findall() with re.compile()/match()
    The re module of MicroPython does not support findall().

--- bitstruct.py
+++ bitstruct.py
@@ -16,28 +16,28 @@ class Error(Exception):
 def _parse_format(fmt):
     if fmt and fmt[-1] in '><':
         byte_order = fmt[-1]
-        fmt = fmt[:-1]
+        tail = fmt[:-1]
     else:
         byte_order = ''
+        tail = fmt
 
-    parsed_infos = re.findall(r'([<>]?)([a-zA-Z])(\d+)', fmt)
-
-    if ''.join([''.join(info) for info in parsed_infos]) != fmt:
-        raise Error("bad format '{}'".format(fmt + byte_order))
+    regex = re.compile(r'([<>]?)([a-zA-Z])(\d+)')
 
     # Use big endian as default and use the endianness of the previous
     # value if none is given for the current value.
     infos = []
     endianness = ">"
 
-    for info in parsed_infos:
-        if info[0] != "":
-            endianness = info[0]
-
-        if info[1] not in 'supPfbtr':
-            raise Error("bad char '{}' in format".format(info[1]))
-
-        infos.append((info[1], int(info[2]), endianness))
+    while tail:
+        info = regex.match(tail)
+        if not info:
+            raise Error("bad format '{}'".format(fmt))
+        endianness = info.group(1) or endianness
+        type_ = info.group(2)
+        if type_ not in 'supPfbtr':
+            raise Error("bad char '{}' in format".format(type_))
+        infos.append((type_, int(info.group(3)), endianness))
+        tail = tail[len(info.group(0)):]
 
     return infos, byte_order or '>'
 
